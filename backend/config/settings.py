43388119386de43f55e46d9661c8496ef2bc7c4c
Sam-eff <richard.effiong2000@gmail.com>
2025-11-22 08:59:30 +0100

    Fix Django admin templates

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -71,10 +71,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [REACT_BUILD_DIR],
+        'DIRS': [REACT_BUILD_DIR] if REACT_BUILD_DIR.exists() else [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
