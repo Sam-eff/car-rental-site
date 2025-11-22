eb394712731b21fd8fff0422e5173d4e7779332c
Sam-eff <richard.effiong2000@gmail.com>
2025-11-22 23:42:38 +0100

    Add makemigrations to build script

--- backend/build.sh
+++ backend/build.sh
@@ -8,6 +8,9 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
+# Make migrations for any model changes
+python manage.py makemigrations
+
 # Run migrations
 python manage.py migrate
 
